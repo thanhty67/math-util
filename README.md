5aba7100dd79073ae2977ce1a7c4da50609eb63c
thanhty67 <th.ty6701@gmail.com>
2022-06-15 11:55:03 +0700

    Refactor the main code and GREEN 4EVER always

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### You win find in this repo the following stuff: 
 * The Math-Util library that offers multiple math functions (a Java-based source code)
 * Unit Testing source code with JUnit framework
+* GitHub Action to demonstrate the Continuos Intergration (CI) technique
+* Regression Test
 * ...
 
 ### Screenshots
--- src/com/tymt/mathutil/core/MathUtil.java
+++ src/com/tymt/mathutil/core/MathUtil.java
@@ -14,7 +14,7 @@ public class MathUtil {
     //Clone class Math của JDK
     //Hàm thư viện dùng chung mà ko cần lưu lại trạng thái/giá trị
     //ta sẽ thiết kế là hàm STATIC
-    
+
     //hàm tính giai thừa!!!
     //n! = 1.2.3.4
     //KO có giai thừa số âm
@@ -23,15 +23,38 @@ public class MathUtil {
     //20 giai thừa 18 con số 0, vừa đủ kiểu long 
     //21 giai thưa tràn kiểu long
     //quy ước 0!...20!
+    
+//    public static long getFactiorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid Arrgument. N mus in 0...20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1; //kết thúc cuộc chơi nhận vào các đầu vào đặc biệt
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;//product = product * i;
+//        }
+//        return product;
+//    }
+
+    //Học đẹ quy trong 30s - RECURSION
+    //Hiện tượng gọi lại chính mình với 1 quy mô khác
+    //Ví dụ: Con búp bê Nga, giống nhau và lồng trong nhau
+    //búp bê to, nhỏ hơn, nhỏ hơn nữa 
+    //tính dùm tui 6!
+    //6! = 6*5!
+    //5! = 5 * 4!
+    //......
+    //1! = 1;
+    //Chốt hạ: n! = n * (n-1)!
     public static long getFactiorial(int n) {
-        if (n < 0 || n > 20) 
-            throw new IllegalArgumentException("Invalid Arrgument. N mus in 0...20");
-        if (n == 0 || n == 1)
+        
+        if (n == 0 || n == 1) {
             return 1; //kết thúc cuộc chơi nhận vào các đầu vào đặc biệt
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;//product = product * i;
         }
-        return product;
+        
+        return n * getFactiorial(n - 1);
     }
+
 }
